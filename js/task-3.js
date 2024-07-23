29e1a76835ac9df5cc8188f9a72605d313cd1aca
Mishagul <169081080+Mishagul@users.noreply.github.com>
2024-07-23 10:13:38 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,17 @@
 function checkForSpam(message) {
-    var lowerCaseMessage = message.toLowerCase();
+    let lowerCaseMessage = message.toLowerCase();
 
-    var spamWords = ["sale", "offer", "amazing", "earn fast money", "[spam]"];
+    let spamWords = ['sale', 'spam',];
 
-    for (var i = 0; i < spamWords.length; i++) {
-        if (lowerCaseMessage.includes(spamWords[i])) {
+    for (let word of spamWords) {
+        if (lowerCaseMessage.includes(word)) {
+            return true;
         }
     }
 
     return false;
 }
+
 
 console.log(checkForSpam("Latest technology news"));
 console.log(checkForSpam("JavaScript weekly newsletter"));
@@ -18,4 +20,3 @@ console.log(checkForSpam("Amazing SalE, only tonight!"));
 console.log(checkForSpam("Trust me, this is not a spam message"));
 console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
 console.log(checkForSpam("[SPAM] How to earn fast money?"));
-
