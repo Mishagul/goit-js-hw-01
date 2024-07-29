1eb5778f83fe6a0d18e8cb4b0a1e15af01b25edc
Mishagul <169081080+Mishagul@users.noreply.github.com>
2024-07-29 12:06:58 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,10 @@
-function checkForSpam(message) {
-    let lowerCaseMessage = message.toLowerCase();
-
-    let spamWords = ['sale', 'spam',];
-
-    for (let word of spamWords) {
-        if (lowerCaseMessage.includes(word)) {
-            return true;
-        }
-    }
-
-    return false;
+function filterArray(numbers, value) {
+const filteredNumbers = numbers.filter(number => number > value);
+return filteredNumbers;
 }
 
-
-console.log(checkForSpam("Latest technology news"));
-console.log(checkForSpam("JavaScript weekly newsletter"));
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Amazing SalE, only tonight!"));
-console.log(checkForSpam("Trust me, this is not a spam message"));
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
